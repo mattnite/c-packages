3a7085961f139b1acc358c3dcaf748ee38e49810
Matt Knight <mattnite@proton.me>
2024-02-21 01:14:24 -0800

    add libyaml and add libcyaml

---
+++ projects/libcyaml/build.zig
@@ -0,0 +1,98 @@
+const std = @import("std");
+
+pub const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .x86, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .musl },
+
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+    .{ .cpu_arch = .aarch64, .os_tag = .windows },
+};
+
+const flags: []const []const u8 = &.{
+    "-std=c11",
+    "-Wall",
+    "-Wextra",
+    "-pedantic",
+    "-Wconversion",
+    "-Wwrite-strings",
+    "-Wcast-align",
+    "-Wpointer-arith",
+    "-Winit-self",
+    "-Wshadow",
+    "-Wstrict-prototypes",
+    "-Wmissing-prototypes",
+    "-Wredundant-decls",
+    "-Wundef",
+    "-Wvla",
+    "-Wdeclaration-after-statement",
+};
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const libyaml_dep = b.dependency("libyaml", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const cyaml = b.addStaticLibrary(.{
+        .name = "cyaml",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    cyaml.defineCMacro("VERSION_MAJOR", "1");
+    cyaml.defineCMacro("VERSION_MINOR", "4");
+    cyaml.defineCMacro("VERSION_PATCH", "1");
+    cyaml.defineCMacro("VERSION_DEVEL", "0");
+    cyaml.addIncludePath(.{ .path = "c/include" });
+    cyaml.addCSourceFiles(.{
+        .files = &.{
+            "c/src/free.c",
+            "c/src/load.c",
+            "c/src/mem.c",
+            "c/src/save.c",
+            "c/src/utf8.c",
+            "c/src/util.c",
+        },
+        .flags = flags,
+    });
+    cyaml.linkLibrary(libyaml_dep.artifact("yaml"));
+
+    cyaml.installHeader("c/include/cyaml/cyaml.h", "cyaml/cyaml.h");
+    b.installArtifact(cyaml);
+
+    const test_exe = b.addExecutable(.{
+        .name = "test-cyaml",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    test_exe.addCSourceFiles(.{
+        .files = &.{
+            "c/test/units/free.c",
+            "c/test/units/load.c",
+            "c/test/units/test.c",
+            "c/test/units/util.c",
+            "c/test/units/errs.c",
+            "c/test/units/file.c",
+            "c/test/units/save.c",
+            "c/test/units/utf8.c",
+        },
+        .flags = flags,
+    });
+    test_exe.linkLibrary(cyaml);
+
+    const test_run = b.addRunArtifact(test_exe);
+    test_run.setCwd(.{ .path = "c" });
+    const test_step = b.step("test", "Run test executable");
+    test_step.dependOn(&test_run.step);
+}
---
+++ projects/libyaml/build.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+const Build = std.Build;
+
+pub const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .x86, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .musl },
+
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+    .{ .cpu_arch = .aarch64, .os_tag = .windows },
+};
+
+pub fn build(b: *Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const libyaml = b.addStaticLibrary(.{
+        .name = "yaml",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    libyaml.defineCMacro("YAML_VERSION_MAJOR", "0");
+    libyaml.defineCMacro("YAML_VERSION_MINOR", "2");
+    libyaml.defineCMacro("YAML_VERSION_PATCH", "5");
+    libyaml.defineCMacro("YAML_VERSION_STRING", "\"0.2.5\"");
+    libyaml.defineCMacro("YAML_DECLARE_STATIC", "1");
+    libyaml.addIncludePath(.{ .path = "c/include" });
+    libyaml.addCSourceFiles(.{
+        .files = &.{
+            "c/src/api.c",
+            "c/src/dumper.c",
+            "c/src/emitter.c",
+            "c/src/loader.c",
+            "c/src/parser.c",
+            "c/src/reader.c",
+            "c/src/scanner.c",
+            "c/src/writer.c",
+        },
+        .flags = &.{},
+    });
+    libyaml.installHeader("c/include/yaml.h", "yaml.h");
+    b.installArtifact(libyaml);
+}
