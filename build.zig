c59a7b3d7bb3b2e1fa8fbe6929576213a9840988
Matt Knight <mattnite@proton.me>
2024-02-23 00:31:23 -0800

    add zlib and enable running tests in CI

--- build.zig
+++ build.zig
@@ -7,10 +7,13 @@ const projects = .{
     .{ "ebur128", @import("ebur128") },
     .{ "libyaml", @import("libyaml") },
     .{ "libcyaml", @import("libcyaml") },
+    .{ "zlib", @import("zlib") },
 };
 
 pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
+
+    // build each project for each advertised target
     inline for (projects) |entry| {
         for (entry[1].targets) |target| {
             const dep = b.dependency(entry[0], .{
@@ -19,6 +22,27 @@ pub fn build(b: *Build) void {
             });
 
             b.getInstallStep().dependOn(dep.builder.getInstallStep());
+        }
+    }
+
+    // setup tests
+    const test_step = b.step("test", "Run available tests");
+    inline for (.{
+        .Debug,
+        .ReleaseSafe,
+        .ReleaseSmall,
+        .ReleaseFast,
+    }) |test_optimize| {
+        inline for (projects) |entry| {
+            const dep = b.dependency(entry[0], .{
+                .optimize = test_optimize,
+            });
+            const project_test = dep.builder.top_level_steps.get("test") orelse {
+                @panic(std.fmt.comptimePrint("project '{s}' does not have a test step in its build.zig", .{
+                    entry[0],
+                }));
+            };
+            test_step.dependOn(&project_test.step);
         }
     }
 
---
+++ projects/zlib/build.zig
@@ -0,0 +1,94 @@
+const std = @import("std");
+const Build = std.Build;
+
+pub const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .x86, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .musl },
+
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+    .{ .cpu_arch = .aarch64, .os_tag = .windows },
+};
+
+pub fn build(b: *Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const z = b.addStaticLibrary(.{
+        .name = "z",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    z.addCSourceFiles(.{
+        .files = &.{
+            "c/adler32.c",
+            "c/compress.c",
+            "c/crc32.c",
+            "c/deflate.c",
+            "c/gzclose.c",
+            "c/gzlib.c",
+            "c/gzread.c",
+            "c/gzwrite.c",
+            "c/inflate.c",
+            "c/infback.c",
+            "c/inftrees.c",
+            "c/inffast.c",
+            "c/trees.c",
+            "c/uncompr.c",
+            "c/zutil.c",
+        },
+        .flags = &.{"-std=c89"},
+    });
+    z.installHeader("c/zlib.h", "zlib.h");
+    z.installHeader("c/zconf.h", "zconf.h");
+    b.installArtifact(z);
+
+    const mod = b.addModule("z", .{
+        .root_source_file = .{ .path = "zig/bindings.zig" },
+    });
+    mod.linkLibrary(z);
+
+    const example_exe = b.addExecutable(.{
+        .name = "example",
+        .link_libc = true,
+        .target = b.host,
+    });
+    example_exe.linkLibrary(z);
+    example_exe.addCSourceFile(.{
+        .file = .{ .path = "c/test/example.c" },
+        .flags = &.{},
+    });
+
+    const example_run = b.addRunArtifact(example_exe);
+    _ = example_run.captureStdOut();
+
+    const example64_exe = b.addExecutable(.{
+        .name = "example64",
+        .link_libc = true,
+        .target = b.host,
+    });
+    example64_exe.linkLibrary(z);
+    example64_exe.addCSourceFile(.{
+        .file = .{ .path = "c/test/example.c" },
+        .flags = &.{"-D_FILE_OFFSET_BITS=64"},
+    });
+
+    const example64_run = b.addRunArtifact(example64_exe);
+    _ = example64_run.captureStdOut();
+
+    const module_test = b.addTest(.{
+        .root_source_file = .{ .path = "zig/bindings.zig" },
+    });
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&example_run.step);
+    test_step.dependOn(&example64_run.step);
+    test_step.dependOn(&module_test.step);
+}
