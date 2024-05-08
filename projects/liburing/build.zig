7d7260efe90534a174a2a87258446b9af7ceb854
Matt Knight <mattnite@proton.me>
2024-05-08 01:22:55 -0700

    drop header generation executables

--- projects/liburing/build.zig
+++ projects/liburing/build.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const Build = std.Build;
 
 const version = .{
-    .major = "2",
-    .minor = "5",
+    .major = 2,
+    .minor = 5,
 };
 
 pub const targets: []const std.Target.Query = &.{
@@ -28,39 +28,19 @@ pub fn build(b: *Build) void {
     const have_futexv = b.option(bool, "have_futexv", "") orelse true;
     const have_idtype_t = b.option(bool, "have_idtype_t", "") orelse true;
 
-    const generate_version_h = b.addExecutable(.{
-        .name = "generate-version-h",
-        .root_source_file = .{ .path = "zig/generate_version_h.zig" },
-        .target = b.host,
-    });
+    const writefiles = b.addWriteFiles();
+    const compat_h = write_compat_h(b, writefiles, .{
+        .have_kernel_rwf_t = have_kernel_rwf_t,
+        .have_kernel_timespec = have_kernel_timespec,
+        .have_open_how = have_open_how,
+        .have_futexv = have_futexv,
+        .have_idtype_t = have_idtype_t,
+    }) catch @panic("OOM");
 
-    const run_version_h = b.addRunArtifact(generate_version_h);
-    run_version_h.addArgs(&.{
-        "--version-major", version.major,
-        "--version-minor", version.minor,
-    });
-    run_version_h.addArg("-o");
-    const version_h = run_version_h.addOutputFileArg("io_uring_version.h");
-
-    const generate_compat_h = b.addExecutable(.{
-        .name = "generate-compat-h",
-        .root_source_file = .{ .path = "zig/generate_compat_h.zig" },
-        .target = b.host,
-    });
-
-    const run_compat_h = b.addRunArtifact(generate_compat_h);
-    if (have_kernel_rwf_t)
-        run_compat_h.addArg("--have-kernel-rwf-t");
-    if (have_kernel_timespec)
-        run_compat_h.addArg("--have-kernel-timespec");
-    if (have_open_how)
-        run_compat_h.addArg("--have-open-how");
-    if (have_futexv)
-        run_compat_h.addArg("--have-futexv");
-    if (have_idtype_t)
-        run_compat_h.addArg("--have-idtype-t");
-    run_compat_h.addArg("-o");
-    const compat_h = run_compat_h.addOutputFileArg("compat.h");
+    const version_h = write_version_h(b, writefiles, .{
+        .major = version.major,
+        .minor = version.minor,
+    }) catch @panic("OOM");
 
     const uring = b.addStaticLibrary(.{
         .name = "uring",
@@ -92,6 +72,129 @@ pub fn build(b: *Build) void {
 
     // a test step is needed for c-packages, but it ultimately does nothing for this project
     _ = b.step("test", "Run tests");
+}
+
+const VersionHeaderOptions = struct {
+    major: u32,
+    minor: u32,
+};
+
+fn write_version_h(b: *Build, writefile: *Build.Step.WriteFile, opts: VersionHeaderOptions) !Build.LazyPath {
+    var text = std.ArrayList(u8).init(b.allocator);
+    defer text.deinit();
+
+    try text.writer().print(
+        \\/* SPDX-License-Identifier: MIT */
+        \\#ifndef LIBURING_VERSION_H
+        \\#define LIBURING_VERSION_H
+        \\
+        \\#define IO_URING_VERSION_MAJOR {}
+        \\#define IO_URING_VERSION_MINOR {}
+        \\
+        \\#endif
+        \\
+    ,
+        .{ opts.major, opts.minor },
+    );
+
+    return writefile.add("io_uring_version.h", try text.toOwnedSlice());
+}
+
+const CompatHeaderOptions = struct {
+    have_kernel_rwf_t: bool,
+    have_kernel_timespec: bool,
+    have_open_how: bool,
+    have_futexv: bool,
+    have_idtype_t: bool,
+};
+
+fn write_compat_h(b: *Build, writefile: *Build.Step.WriteFile, opts: CompatHeaderOptions) !Build.LazyPath {
+    var text = std.ArrayList(u8).init(b.allocator);
+    defer text.deinit();
+
+    const writer = text.writer();
+    try writer.writeAll(
+        \\/* SPDX-License-Identifier: MIT */
+        \\#ifndef LIBURING_COMPAT_H
+        \\#define LIBURING_COMPAT_H
+        \\
+        \\
+    );
+
+    if (!opts.have_kernel_rwf_t)
+        try writer.writeAll("typedef int __kernel_rwf_t;\n\n");
+
+    if (!opts.have_kernel_timespec)
+        try writer.writeAll(
+            \\#include <stdint.h>
+            \\
+            \\struct __kernel_timespec {
+            \\	int64_t		tv_sec;
+            \\	long long	tv_nsec;
+            \\};
+            \\
+            \\/* <linux/time_types.h> is not available, so it can't be included */
+            \\#define UAPI_LINUX_IO_URING_H_SKIP_LINUX_TIME_TYPES_H 1
+            \\
+            \\
+        )
+    else
+        try writer.writeAll(
+            \\#include <linux/time_types.h>
+            \\/* <linux/time_types.h> is included above and not needed again */
+            \\#define UAPI_LINUX_IO_URING_H_SKIP_LINUX_TIME_TYPES_H 1
+            \\
+        );
+
+    if (!opts.have_open_how)
+        try writer.writeAll(
+            \\#include <inttypes.h>
+            \\
+            \\struct open_how {
+            \\	uint64_t	flags;
+            \\	uint64_t	mode;
+            \\	uint64_t	resolve;
+            \\};
+            \\
+            \\
+        )
+    else
+        try writer.writeAll("#include <linux/openat2.h>\n\n");
+
+    try writer.writeAll("#include <sys/stat.h>\n\n");
+
+    if (!opts.have_futexv)
+        try writer.writeAll(
+            \\#include <inttypes.h>
+            \\
+            \\#define FUTEX_32	2
+            \\#define FUTEX_WAITV_MAX	128
+            \\
+            \\struct futex_waitv {
+            \\	uint64_t	val;
+            \\	uint64_t	uaddr;
+            \\	uint32_t	flags;
+            \\	uint32_t	__reserved;
+            \\};
+            \\
+            \\
+        );
+
+    if (!opts.have_idtype_t)
+        try writer.writeAll(
+            \\typedef enum
+            \\{
+            \\  P_ALL,		/* Wait for any child.  */
+            \\  P_PID,		/* Wait for specified process.  */
+            \\  P_PGID		/* Wait for members of process group.  */
+            \\} idtype_t;
+            \\
+            \\
+        );
+
+    try writer.writeAll("#endif\n");
+
+    return writefile.add("compat.h", try text.toOwnedSlice());
 }
 
 const srcs = &.{
